e0df3d4df90f0600c6fd594b6ce215021229b98c
Tapakorn086 <nakason_t@silpakorn.edu>
2025-03-21 10:45:40 +0700

    create: add calculate_ot function to compute overtime pay

--- ot_calculator.py
+++ ot_calculator.py
@@ -21,4 +21,32 @@ def get_ot_rate(day, start_time, end_time):
         elif 21 <= start_hour < 24 and 21 <= end_hour <= 24:
             return 3.0  
     return "กรุณาตรวจสอบเวลาการทำงานของคุณอีกครั้ง"
+
+
+def calculate_ot(employee_name, hourly_rate,day, ot_time_start, ot_time_end):
+    start_hour, start_minute = map(int, ot_time_start.split(':'))
+    end_hour, end_minute = map(int, ot_time_end.split(':'))
     
+    start_time_decimal = start_hour + (start_minute / 60)
+    end_time_decimal = end_hour + (end_minute / 60)
+    print("start_time_decimal:", start_time_decimal)
+    print("end_time_decimal:", end_time_decimal)
+
+    ot_hours = end_time_decimal - start_time_decimal
+    print(ot_hours)
+    ot_rate = get_ot_rate(day, ot_time_start, ot_time_end)
+    ot_pay = hourly_rate * ot_hours * ot_rate
+    print(ot_rate)
+    print(ot_pay)
+    print("===========")
+    return {
+        "พนักงาน": employee_name,
+        "จำนวนชั่วโมง OT": ot_hours,
+        "อัตรา OT": ot_rate,
+        "ค่า OT": ot_pay
+    }
+    
+result_A = calculate_ot("สมชาย", 100, "จันทร์", "18:00", "20:00")
+result_B = calculate_ot("สมชาย", 100, "เสาร์", "18:00", "20:00")
+print(result_A)
+print(result_B)
