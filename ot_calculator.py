658173419d1e3550d84ca45db81f93f1368224e9
Tapakorn086 <nakason_t@silpakorn.edu>
2025-03-21 10:42:43 +0700

    refactor: update get_ot_rate function to handle day and time ranges

--- ot_calculator.py
+++ ot_calculator.py
@@ -1,10 +1,24 @@
-def get_ot_rate(time_str):
-    hour = int(time_str.split(':')[0])
-    print(hour)
-    if 21 <= hour < 24:
-        return 3
-    else:
-        return 1.5
-
-result = get_ot_rate("18:00")
-print("OT Rate:", result)
+def get_ot_rate(day, start_time, end_time):
+    normalday = ["จันทร์", "อังคาร", "พุธ", "พฤหัสบดี", "ศุกร์"]
+    weekend = ["เสาร์", "อาทิตย์"]
+    
+    start_hour = int(start_time.split(':')[0])
+    end_hour = int(end_time.split(':')[0])
+    
+    if day in normalday:
+        if 9 <= start_hour < 18 and 9 <= end_hour <= 18:
+            return 1.5 
+        elif 18 <= start_hour < 21 and 18 <= end_hour <= 21:
+            return 1.5 
+        elif 21 <= start_hour < 24 and 21 <= end_hour <= 24:
+            return 3.0 
+    
+    elif day in weekend:
+        if 9 <= start_hour < 18 and 9 <= end_hour <= 18:
+            return 1.5 
+        elif 18 <= start_hour < 21 and 18 <= end_hour <= 21:
+            return 1.5  
+        elif 21 <= start_hour < 24 and 21 <= end_hour <= 24:
+            return 3.0  
+    return "กรุณาตรวจสอบเวลาการทำงานของคุณอีกครั้ง"
+    
